6eafa4e9756ff6482c6dad2b651d68a16a4674a7
Niels <info@heimseiten.de>
2022-08-22 13:04:23 +0200

    Unlocked Contao 5

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -11,29 +11,14 @@ use Contao\Module;
 class HooksListener
 {
     
-    public function onCompileArticle(FrontendTemplate $objTemplate, array $arrData, Module $module): void
-    {
-        if (TL_MODE == 'FE' && $objTemplate->type == 'article') {
-            $mod_article_before_content_elements = new FrontendTemplate('mod_article_before_content_elements');
-            $mod_article_before_content_elements->articleMinHeight = $arrData['articleMinHeight'];
-            $mod_article_before_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
-            $elements = $objTemplate->elements;
-            array_unshift($elements, $mod_article_before_content_elements->parse());
-
-            $mod_article_after_content_elements = new FrontendTemplate('mod_article_after_content_elements');
-            $mod_article_after_content_elements->articleMaxWidth = $arrData['articleMaxWidth'];
-            array_push($elements, $mod_article_after_content_elements->parse());
-            
-            $objTemplate->elements = $elements;
-        }
-    }
-
     public function onParseTemplate(Template $objTemplate)
     {
-        if (TL_MODE == 'FE' && $objTemplate->type == 'article') {
-            if ($objTemplate->articleMinHeight) { $objTemplate->style .= '--article_min_height:' . $objTemplate->articleMinHeight . ';'; }
+        if ($objTemplate->type == 'article') {
+            if ($objTemplate->articleMinHeight) { 
+                $objTemplate->style .= '--article_min_height:' . $objTemplate->articleMinHeight . ';'; 
+            }
             if ($objTemplate->articleMaxWidth) { 
-                $objTemplate->class .= ' has_inside';
+                $objTemplate->style .= '--article_inside_width:' . $objTemplate->articleMaxWidth . ';'; 
                 if ( $objTemplate->articleMaxWidth == '100%' || $objTemplate->articleMaxWidth == '100vw' ) {
                     $objTemplate->class .= ' full_width';
                 }
